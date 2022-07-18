917f16e909c46c44f4a5a1ea7dcf97a6bd33f4ae
Deyvison Tavares <deyvisontav@gmail.com>
2022-07-18 13:12:36 -0300

    first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,46 @@
+import todoLogo from "../../assets/todoLogo.svg";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import styles from "./header.module.css";
+import { ChangeEvent, FormEvent, useState, useRef } from "react";
+
+interface Props {
+  onAddTask: (taskTitle: string) => void;
+}
+
+export function Header({ onAddTask }: Props) {
+  const [title, setTitle] = useState("");
+  const letterInputRef = useRef(null as any)
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    onAddTask(title);
+    setTitle("");
+    letterInputRef.current.focus()
+  }
+
+  function onChangeTitle(event: ChangeEvent<HTMLInputElement>) {
+    setTitle(event.target.value);
+  }
+
+  return (
+    
+    <header className={styles.header}>
+      <img src={todoLogo} />
+
+      <form onSubmit={handleSubmit} className={styles.newTaskForm}>
+        <input
+          placeholder="Adicione uma nova tarefa"
+          type="text"
+          value={title}
+          onChange={onChangeTitle}
+          ref={letterInputRef}
+        />
+        <button >
+          Criar
+          <AiOutlinePlusCircle size={20} />
+        </button>
+      </form>
+    </header>
+  );
+}
